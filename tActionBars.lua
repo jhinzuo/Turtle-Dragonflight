24554d2d4d50491bc9436b9abb9fe2cce0c75227
jhinzuo <68930317+jhinzuo@users.noreply.github.com>
2025-04-14 22:28:35 +0200

    Update tActionBars.lua to not rely on OnUpdate
    A needed fix to be able to use CleveRoidMacros and Turtle_DF otherwise it makes the additional actionbars flicker.

--- tActionBars.lua
+++ tActionBars.lua
@@ -1,81 +1,33 @@
-
-
 for _, g in pairs({MainMenuBarTexture1, MainMenuBarTexture0, BonusActionBarTexture1, BonusActionBarTexture0, BonusActionBarTexture2}) do
     if g then
         g:SetTexture("")
     end
 end
 
-local frameBotLeft = CreateFrame("Frame")
-frameBotLeft:SetScript("OnUpdate", function()
+local function processActionBarButtons(barName, buttonPrefix, xOffset)
+    local bar = getglobal(barName)
+    if not bar then return end
+
     for i = 1, 12 do
-        local button = getglobal("MultiBarBottomLeftButton" .. i)
+        local button = getglobal(buttonPrefix .. i)
         if button then
             button:SetWidth(36)
             button:SetHeight(36)
             button:SetNormalTexture("Interface\\Addons\\Turtle-Dragonflight\\img\\uiactionbariconframe.tga")
             button:SetHighlightTexture("Interface\\Addons\\Turtle-Dragonflight\\img\\uiactionbariconframehighlight.tga")
-            local texture = button:GetNormalTexture()
-            texture:SetWidth(36)
-            texture:SetHeight(36)
+            button:SetFrameStrata("MEDIUM")
         end
     end
-end)
+end
 
-local frameRight = CreateFrame("Frame")
-frameRight:SetScript("OnUpdate", function()
-    for i = 1, 12 do
-        local button = getglobal("MultiBarRightButton" .. i)
-        if button then
-            button:SetWidth(36)
-            button:SetHeight(36)
-            button:SetNormalTexture("Interface\\Addons\\Turtle-Dragonflight\\img\\uiactionbariconframe.tga")
-            button:SetHighlightTexture("Interface\\Addons\\Turtle-Dragonflight\\img\\uiactionbariconframehighlight.tga")
-            local texture = button:GetNormalTexture()
-            texture:SetWidth(36)
-            texture:SetHeight(36)
-        end
-    end
-end)
+processActionBarButtons("MultiBarBottomLeft", "MultiBarBottomLeftButton", -125)
+processActionBarButtons("MultiBarRight", "MultiBarRightButton", 127)
+processActionBarButtons("MultiBarLeft", "MultiBarLeftButton", -380)
+processActionBarButtons("MultiBarBottomRight", "MultiBarBottomRightButton", 380)
 
-local frameBotRight2 = CreateFrame("Frame")
-frameBotRight2:SetScript("OnUpdate", function()
-    for i = 1, 12 do
-        local button = getglobal("MultiBarLeftButton" .. i)
-        if button then
-            button:SetWidth(36)
-            button:SetHeight(36)
-            button:SetNormalTexture("Interface\\Addons\\Turtle-Dragonflight\\img\\uiactionbariconframe.tga")
-            button:SetHighlightTexture("Interface\\Addons\\Turtle-Dragonflight\\img\\uiactionbariconframehighlight.tga")
-            local texture = button:GetNormalTexture()
-            texture:SetWidth(36)
-            texture:SetHeight(36)
-        end
-    end
-end)
-
-local frameBotRight = CreateFrame("Frame")
-frameBotRight:SetScript("OnUpdate", function()
-    for i = 1, 12 do
-        local button = getglobal("MultiBarBottomRightButton" .. i)
-        if button then
-            button:SetWidth(36)
-            button:SetHeight(36)
-            button:SetNormalTexture("Interface\\Addons\\Turtle-Dragonflight\\img\\uiactionbariconframe.tga")
-            button:SetHighlightTexture("Interface\\Addons\\Turtle-Dragonflight\\img\\uiactionbariconframehighlight.tga")
-            local texture = button:GetNormalTexture()
-            texture:SetWidth(36)
-            texture:SetHeight(36)
-        end
-    end
-end)
-
--- Get the right action bar frame
 local rightActionBar = MultiBarRight
-
--- Get the current position of the right action bar
-local point, relativeTo, relativePoint, xOfs, yOfs = rightActionBar:GetPoint()
-
--- Reposition the right action bar 10 pixels up
-rightActionBar:ClearAllPoints()
-rightActionBar:SetPoint(point, relativeTo, relativePoint, xOfs, yOfs + 10)
+if rightActionBar then
+    local point, relativeTo, relativePoint, xOfs, yOfs = rightActionBar:GetPoint()
+    rightActionBar:ClearAllPoints()
+    rightActionBar:SetPoint(point, relativeTo, relativePoint, xOfs, yOfs + 10);
+end
